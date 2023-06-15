1d754a2e75824d4bb753099aebe9f5d7186a6ed7
rayoh2018 <rayoh2018@gmail.com>
2023-06-15 13:58:36 -0700

    a script that generates the assembly code (Intel syntax) of a C code and save it in an output fil

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - a main function that outputs using  with puts function
+ * main - a main function that outputs using with puts function
  * return: 0 (if its a sucess)
  */
 int main(void)
