6c368689cf16b0dfeb262e30069934488bfa0588
rayoh2018 <rayoh2018@gmail.com>
2023-06-16 16:44:29 -0700

    combinationd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,17 +6,17 @@
 int main(void)
 {
 int n, m, l;
-for (n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 {
- for (m = n + 1; m < 10; m++)
-{
-for (l = m + 1; l < 10; l++)
+	for (m = n + 1; m < 10; m++)
+	{
+	for (l = m + 1; l < 10; l++)
 {
 putchar('0' + n);
 putchar('0' + m);
 putchar('0' + l);
-if (n != 7 || m != 8)
-{
+	if (n != 7 || m != 8)
+	{
 putchar(',');
 putchar(' ');
 }
