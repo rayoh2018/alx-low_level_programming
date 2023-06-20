ab7026498fcc4ab5b8d43ffe916c97371bf8cd88
rayoh2018 <rayoh2018@gmail.com>
2023-06-20 14:40:15 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif 
