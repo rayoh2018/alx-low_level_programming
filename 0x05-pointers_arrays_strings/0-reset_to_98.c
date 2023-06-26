70a135bceb551285d708aac8d76b04580ad79fd5
rayoh2018 <rayoh2018@gmail.com>
2023-06-26 07:33:31 -0700

    retrial

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 /**
- * reset_to_98 - takes a pointer to an int parameter and updates the value to 98
+ * reset_to_98 - takes a pointer to an int parameter and updates to 98
  *
  * @n: int parameter
- *
  * Return: Nothing
  */
-
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
